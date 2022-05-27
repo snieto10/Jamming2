6d70dc9b25cb839df15a7a0798519fe3f6836fd7
Sergio Nieto <snieto10@gmail.com>
2022-05-27 15:02:48 -0400

    section 2 finished

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults'
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
   render() {
@@ -13,7 +14,7 @@ class App extends React.Component {
         <SearchBar />
         <div className="App-playlist">
         <SearchResults />
-        {/*<Playlist />*/}
+        <Playlist />
       </div>
     </div>
   </div>
