25b54336b2a7913f607852f5adf06ef6be5ac2e3
Sergio Nieto <snieto10@gmail.com>
2022-05-31 14:15:28 -0400

    section 7 step 61 completed

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,10 +5,19 @@ import TrackList from '../TrackList/TrackList'
 
 
 class Playlist extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.handleNameChange = this.handleNameChange.bind(this)
+    }
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value)
+    }
     render () {
         return (
             <div className="Playlist">
-                <input defaultValue={"New playlist"}/>
+                <input defaultValue={"New playlist"}
+                onChange={this.handleNameChange} />
                 <TrackList 
                     tracks={this.props.playlistTracks}
                     onRemove={this.props.onRemove}
