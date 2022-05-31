c3893abdd29fe9626897152dab70000e647902f4
Sergio Nieto <snieto10@gmail.com>
2022-05-31 10:23:47 -0400

    step 35

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,4 +1,4 @@
-import React from React
+import React from 'react'
 import './Track.css'
 
 class Track extends React.Component {
@@ -13,8 +13,8 @@ class Track extends React.Component {
         return (
             <div className="Track">
                     <div className="Track-information">
-                    <h3>track name</h3>
-                    <p>track artist | track album</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album} </p>
                     </div>
                 <button className="Track-action">{this.renderAction()}</button>
             </div>
