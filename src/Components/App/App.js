243558ded88891ccf9e4317f0b98272bb23d019a
Sergio Nieto <snieto10@gmail.com>
2022-06-02 09:50:22 -0400

    step 65 completed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
+    this.savePlaylist = this.savePlaylist.bind(this)
 
   }
 
@@ -48,6 +49,10 @@ class App extends React.Component {
     this.setState({playlistName: name})
   }
 
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri)
+  }
+
   render() {
     
     return (
@@ -63,7 +68,8 @@ class App extends React.Component {
           playlistName={this.state.playlistName} 
           playlistTracks={this.state.playlistTracks}
           onRemove={this.removeTrack}
-          onNameChange={this.updatePlaylistName} />
+          onNameChange={this.updatePlaylistName} 
+          onSave={this.savePlaylist}/>
       </div>    
     </div>
   </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -22,7 +22,7 @@ class Playlist extends React.Component {
                     tracks={this.props.playlistTracks}
                     onRemove={this.props.onRemove}
                     isRemoval={true} />
-                <button class="Playlist-save">SAVE TO SPOTIFY</button>
+                <button class="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
         )
     }
